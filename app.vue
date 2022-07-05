4342d259554d729c601612c179e983db49f6dd09
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-07-05 15:04:52 +0900

    feat: added nuxt supabase package

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+const client = useSupabaseClient()
+
+const { data: tasks } = await useAsyncData('random', () => {
+  return client.from('random').select()
+})
+
+const counter = useState('counter', () => 1)
+
+
+
+</script>
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>some</h1>
+    <button @click="counter++">{{ counter }}</button>
+    <pre>{{ tasks }}</pre>
   </div>
 </template>
