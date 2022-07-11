f354b67bf83745daa46dce26b8b9548e9093f573
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-07-11 13:59:01 +0900

    feat: created account, auth and avatar.vue

--- components/Auth.vue
+++ components/Auth.vue
@@ -1,43 +1,32 @@
+<template>
+    <form class="row flex flex-center" @submit.prevent="handleLogin">
+        <div class="col-6 form-widget">
+            <h1 class="header">Supabase + Vue 3</h1>
+            <p class="description">Sign in via magic link with your email below</p>
+            <div>
+                <input class="inputField" type="email" placeholder="Your email" v-model="email" />
+            </div>
+            <div>
+                <input type="submit" class="button block" :value="loading ? 'Loading' : 'Send magic link'"
+                    :disabled="loading" />
+            </div>
+        </div>
+    </form>
+</template>
+
 <script setup>
-
-const supabase = useSupabaseClient()
-
-const loading = useState('loading', () => false)
-const email = useState('email', () => '')
-
+const loading = ref(false)
+const email = ref("")
 const handleLogin = async () => {
     try {
         loading.value = true
-        console.log(email.value)
-        const { error } = await supabase.auth.signIn({ email })
+        const { error } = await supabase.auth.signIn({ email: email.value })
         if (error) throw error
-        alert('Check your email for the login link!')
+        alert("Check your email for the login link!")
     } catch (error) {
         alert(error.error_description || error.message)
     } finally {
         loading.value = false
     }
 }
-
-const handleOnChange = (event) => {
-    email.value = event.target.value
-}
 </script>
-
-<template>
-    <div className="row flex flex-center">
-        <div className="col-6 form-widget">
-            <h1 className="header">Supabase + Next.js</h1>
-            <p className="description">Sign in via magic link with your email below</p>
-            <div>
-                <input className="inputField" type="email" placeholder="Your email" :value="email"
-                    @change="handleOnChange" />
-            </div>
-            <div>
-                <button @click="handleLogin" className="button block" :disabled="loading">
-                    <span>{{ loading ? 'Loading' : 'Send magic link' }}</span>
-                </button>
-            </div>
-        </div>
-    </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
-<script>
+<template>
+    <div class="container" style="padding: 50px 0 100px 0">
+        <Account v-if="user" />
+        <Auth v-else />
+    </div>
+</template>
 
+<script setup>
 
+const supabase = useSupabaseClient()
 
+const user = useUser();
+
+user.value = useSupabaseUser()
+supabase.auth.onAuthStateChange((_, session) => {
+    user.value = session.user
+})
 
 </script>
-
-<template>
-    <Auth />
-</template>
